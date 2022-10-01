fcae2d27e45386648826bb1ef58897609950475f
Firebelley <firebelley@users.noreply.github.com>
2022-09-30 21:53:29 -0400

    grapple mechanics work

--- scripts/game_object/Grapple.cs
+++ scripts/game_object/Grapple.cs
@@ -6,6 +6,11 @@ namespace Game.GameObject
 {
     public class Grapple : Node2D
     {
+        [Signal]
+        public delegate void ConnectedToPoint(Vector2 point);
+        [Signal]
+        public delegate void Disconnected();
+
         private const int SEGMENT_LENGTH = 4;
         private const float AMP_DECAY = 50f;
         private const float FREQUENCY_DECAY = 3f;
@@ -13,6 +18,7 @@ namespace Game.GameObject
         private float waveAmplitude = 0f;
         private float waveFrequency = 0f;
         private Vector2 connectedPosition;
+        private bool isConnected;
 
         [Node]
         private Line2D line2d;
@@ -28,6 +34,7 @@ namespace Game.GameObject
         // TODO: amp should be tween
         public override void _Process(float delta)
         {
+            if (!isConnected) return;
             waveAmplitude = Mathf.Max(waveAmplitude - (delta * AMP_DECAY), 0f);
             waveFrequency = Mathf.Min(waveFrequency + (delta * FREQUENCY_DECAY), 8f);
             UpdateGrapple();
@@ -35,15 +42,25 @@ namespace Game.GameObject
 
         public void Shoot()
         {
+            if (isConnected)
+            {
+                isConnected = false;
+                ClearGrapple();
+                EmitSignal(nameof(Disconnected));
+                return;
+            }
+
             var direction = this.GetMouseDirection();
             ClearGrapple();
-            var raycast = GetTree().Root.World2d.DirectSpaceState.Raycast(GlobalPosition, direction * 1000f, null, 1 << 0, true, false);
+            var raycast = GetTree().Root.World2d.DirectSpaceState.Raycast(GlobalPosition, GlobalPosition + (direction * 1000f), null, 1 << 0, true, false);
             if (raycast != null)
             {
                 connectedPosition = raycast.Position;
                 waveAmplitude = 15f;
                 waveFrequency = 4f;
+                isConnected = true;
                 UpdateGrapple();
+                EmitSignal(nameof(ConnectedToPoint), connectedPosition);
             }
         }
 
@@ -56,18 +73,17 @@ namespace Game.GameObject
             var length = (connectedPosition - GlobalPosition).Length();
             var segments = Mathf.CeilToInt(length / SEGMENT_LENGTH);
 
-            var initialPoint = GlobalPosition;
+            var initialPoint = Vector2.Zero;
             var strictPoints = new List<Vector2> {
-                line2d.ToLocal(initialPoint)
+                initialPoint
             };
             var wavyPoints = new List<Vector2> {
-                line2d.ToLocal(initialPoint)
+                initialPoint
             };
 
             for (int i = 1; i < segments; i++)
             {
                 var previousStrictPoint = strictPoints[i - 1];
-                // var previousWavyPoint = wavyPoints[i - 1];
                 var strictPoint = previousStrictPoint + (straightLineDirection * SEGMENT_LENGTH);
 
                 var wavyPoint = strictPoint + (perpendicularDirection * Mathf.Sin(i * (Mathf.Pi / waveFrequency)) * waveAmplitude);
--- scripts/game_object/Player.cs
+++ scripts/game_object/Player.cs
@@ -1,12 +1,35 @@
 using Godot;
 using GodotUtilities;
+using GodotUtilities.Logic;
 
 namespace Game.GameObject
 {
     public class Player : KinematicBody2D
     {
+        private const float GROUNDED_SPEED = 200f;
+        private const float ACCELERATION = 20f;
+        private const float AIRBORNE_ACCELERATION = 5f;
+        private const float JUMP_FORCE = 428;
+        private const float GRAVITY = 1200f;
+        private const float JUMP_GRAVITY_MULTIPLIER = 3f;
+        private const float GRAPPLE_FORCE_DECAY = 5f;
+        private const float MAX_GRAPPLE_FORCE = 1200f;
+
         [Node]
         private Grapple grapple;
+
+        private Vector2 velocity;
+        private bool isGrappling = false;
+        private Vector2 connectedPoint;
+        private float currentGrappleForce;
+
+        private enum State
+        {
+            Normal,
+            Airborne,
+            Grappling
+        }
+        private StateMachine<State> stateMachine = new();
 
         public override void _Notification(int what)
         {
@@ -16,12 +39,100 @@ namespace Game.GameObject
             }
         }
 
+        public override void _Ready()
+        {
+            stateMachine.AddState(State.Normal, StateNormal);
+            stateMachine.AddState(State.Airborne, StateAirborne);
+            stateMachine.AddState(State.Grappling, StateGrappling);
+            stateMachine.SetInitialState(StateAirborne);
+            grapple.Connect(nameof(Grapple.ConnectedToPoint), this, nameof(OnGrappleConnected));
+            grapple.Connect(nameof(Grapple.Disconnected), this, nameof(OnGrappleDisconnected));
+        }
+
         public override void _UnhandledInput(InputEvent evt)
         {
             if (evt.IsActionPressed("grapple"))
             {
                 grapple.Shoot();
             }
+        }
+
+        public override void _Process(float delta)
+        {
+            stateMachine.Update();
+        }
+
+        private void StateNormal()
+        {
+            var delta = GetProcessDeltaTime();
+            var moveVec = GetMovementVector();
+
+            velocity.x = Mathf.Lerp(velocity.x, GROUNDED_SPEED * moveVec.x, 1f - Mathf.Exp(-ACCELERATION * delta));
+
+            var gravityMultiplier = 1f;
+
+            if (moveVec.y > 0)
+            {
+                velocity.y = -JUMP_FORCE;
+            }
+
+            if (!Input.IsActionPressed("jump") && velocity.y < 0 && !isGrappling)
+            {
+                gravityMultiplier = JUMP_GRAVITY_MULTIPLIER;
+            }
+
+            velocity.y += GRAVITY * delta * gravityMultiplier;
+            velocity = MoveAndSlideWithSnap(velocity, Vector2.Down, Vector2.Up);
+
+            if (!IsOnFloor())
+            {
+                stateMachine.ChangeState(StateAirborne);
+            }
+        }
+
+        private void StateAirborne()
+        {
+            var delta = GetProcessDeltaTime();
+            var moveVec = GetMovementVector();
+            velocity.x = Mathf.Lerp(velocity.x, GROUNDED_SPEED * moveVec.x, 1f - Mathf.Exp(-AIRBORNE_ACCELERATION * delta));
+            velocity.y += GRAVITY * delta;
+
+            velocity = MoveAndSlideWithSnap(velocity, Vector2.Down, Vector2.Up);
+
+            if (IsOnFloor())
+            {
+                stateMachine.ChangeState(StateNormal);
+            }
+        }
+
+        private void StateGrappling()
+        {
+            var delta = GetProcessDeltaTime();
+            var direction = (connectedPoint - grapple.GlobalPosition).Normalized();
+            currentGrappleForce = Mathf.Lerp(currentGrappleForce, MAX_GRAPPLE_FORCE, 1f - Mathf.Exp(-GRAPPLE_FORCE_DECAY * delta));
+            velocity = velocity.LinearInterpolate(direction * currentGrappleForce, 1f - Mathf.Exp(-GRAPPLE_FORCE_DECAY * delta));
+            velocity = MoveAndSlide(velocity);
+        }
+
+        private Vector2 GetMovementVector()
+        {
+            var x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+            var y = Input.IsActionJustPressed("jump") ? Input.GetActionStrength("jump") : 0;
+            return new Vector2(x, y);
+        }
+
+        private void OnGrappleConnected(Vector2 point)
+        {
+            stateMachine.ChangeState(StateGrappling);
+            currentGrappleForce = 0f;
+            isGrappling = true;
+            connectedPoint = point;
+        }
+
+        private void OnGrappleDisconnected()
+        {
+            isGrappling = false;
+            stateMachine.ChangeState(StateAirborne);
         }
     }
 }
